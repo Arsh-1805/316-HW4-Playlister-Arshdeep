b4be67d3176ea8276a7ef1146546240b8764f0d2
Arshdeep Singh <arsh@arshdeeps-air.mynetworksettings.com>
2025-11-02 23:48:08 -0500

    changed two index.js in order to change from axios to fetch

--- client/src/auth/requests/index.js
+++ client/src/auth/requests/index.js
@@ -1,50 +1,48 @@
 /*
-    This is our http api for all things auth, which we use to 
-    send authorization requests to our back-end API. Note we`re 
-    using the Axios library for doing this, which is an easy to 
-    use AJAX-based library. We could (and maybe should) use Fetch, 
-    which is a native (to browsers) standard, but Axios is easier
-    to use when sending JSON back and forth and it`s a Promise-
-    based API which helps a lot with asynchronous communication.
-    
-    @author McKilla Gorilla
-*/
+ * Auth HTTP API using Fetch (axios-like return shape)
+ */
 
-import axios from 'axios'
-axios.defaults.withCredentials = true;
-const api = axios.create({
-    baseURL: 'http://localhost:4000/auth',
-})
+const API_BASE_URL = "http://localhost:4000/auth";
 
-// THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
-// REQUEST METHOD (like get) AND PATH (like /register). SOME ALSO
-// REQUIRE AN id SO THAT THE SERVER KNOWS ON WHICH LIST TO DO ITS
-// WORK, AND SOME REQUIRE DATA, WHICH WE WE WILL FORMAT HERE, FOR WHEN
-// WE NEED TO PUT THINGS INTO THE DATABASE OR IF WE HAVE SOME
-// CUSTOM FILTERS FOR QUERIES
-
-export const getLoggedIn = () => api.get(`/loggedIn/`);
-export const loginUser = (email, password) => {
-    return api.post(`/login/`, {
-        email : email,
-        password : password
-    })
-}
-export const logoutUser = () => api.get(`/logout/`)
-export const registerUser = (firstName, lastName, email, password, passwordVerify) => {
-    return api.post(`/register/`, {
-        firstName : firstName,
-        lastName : lastName,
-        email : email,
-        password : password,
-        passwordVerify : passwordVerify
-    })
-}
-const apis = {
-    getLoggedIn,
-    registerUser,
-    loginUser,
-    logoutUser
+async function fetchJSON(url, options = {}) {
+  const res = await fetch(url, {
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+    ...options,
+  });
+  const data = await res.json();
+  return { data, status: res.status };
 }
 
-export default apis
+export const getLoggedIn = () =>
+  fetchJSON(`${API_BASE_URL}/loggedIn`, { method: "GET" });
+
+export const loginUser = (email, password) =>
+  fetchJSON(`${API_BASE_URL}/login`, {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+  });
+
+export const logoutUser = () =>
+  fetchJSON(`${API_BASE_URL}/logout`, { method: "GET" });
+
+export const registerUser = (
+  firstName,
+  lastName,
+  email,
+  password,
+  passwordVerify
+) =>
+  fetchJSON(`${API_BASE_URL}/register`, {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+      passwordVerify,
+    }),
+  });
+
+const apis = { getLoggedIn, registerUser, loginUser, logoutUser };
+export default apis;
--- client/src/store/requests/index.js
+++ client/src/store/requests/index.js
@@ -1,51 +1,55 @@
 /*
-    This is our http api, which we use to send requests to
-    our back-end API. Note we`re using the Axios library
-    for doing this, which is an easy to use AJAX-based
-    library. We could (and maybe should) use Fetch, which
-    is a native (to browsers) standard, but Axios is easier
-    to use when sending JSON back and forth and it`s a Promise-
-    based API which helps a lot with asynchronous communication.
-    
-    @author McKilla Gorilla
-*/
+ * Store/Playlist HTTP API using Fetch (axios-like return shape)
+ */
 
-import axios from 'axios'
-axios.defaults.withCredentials = true;
-const api = axios.create({
-    baseURL: 'http://localhost:4000/store',
-})
+const API_BASE_URL = "http://localhost:4000/api";
 
-// THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
-// REQUEST METHOD (like get) AND PATH (like /top5list). SOME ALSO
-// REQUIRE AN id SO THAT THE SERVER KNOWS ON WHICH LIST TO DO ITS
-// WORK, AND SOME REQUIRE DATA, WHICH WE WE WILL FORMAT HERE, FOR WHEN
-// WE NEED TO PUT THINGS INTO THE DATABASE OR IF WE HAVE SOME
-// CUSTOM FILTERS FOR QUERIES
-export const createPlaylist = (newListName, newSongs, userEmail) => {
-    return api.post(`/playlist/`, {
-        // SPECIFY THE PAYLOAD
-        name: newListName,
-        songs: newSongs,
-        ownerEmail: userEmail
-    })
+async function fetchJSON(url, options = {}) {
+  const res = await fetch(url, {
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+    ...options,
+  });
+  const data = await res.json();
+  return { data, status: res.status };
 }
-export const deletePlaylistById = (id) => api.delete(`/playlist/${id}`)
-export const getPlaylistById = (id) => api.get(`/playlist/${id}`)
-export const getPlaylistPairs = () => api.get(`/playlistpairs/`)
-export const updatePlaylistById = (id, playlist) => {
-    return api.put(`/playlist/${id}`, {
-        // SPECIFY THE PAYLOAD
-        playlist : playlist
-    })
-}
+
+/** CREATE NEW PLAYLIST */
+export const createPlaylist = (newListName, songs, ownerEmail, ownerUserName) =>
+  fetchJSON(`${API_BASE_URL}/playlist/`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: newListName,
+      songs,
+      ownerEmail,
+      ownerUserName,
+    }),
+  });
+
+/** DELETE PLAYLIST BY ID */
+export const deletePlaylistById = (id) =>
+  fetchJSON(`${API_BASE_URL}/playlist/${id}`, { method: "DELETE" });
+
+/** GET PLAYLIST BY ID */
+export const getPlaylistById = (id) =>
+  fetchJSON(`${API_BASE_URL}/playlist/${id}`, { method: "GET" });
+
+/** GET PLAYLIST PAIRS */
+export const getPlaylistPairs = () =>
+  fetchJSON(`${API_BASE_URL}/playlistpairs/`, { method: "GET" });
+
+/** UPDATE PLAYLIST BY ID */
+export const updatePlaylistById = (id, updatedPlaylist) =>
+  fetchJSON(`${API_BASE_URL}/playlist/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(updatedPlaylist),
+  });
 
 const apis = {
-    createPlaylist,
-    deletePlaylistById,
-    getPlaylistById,
-    getPlaylistPairs,
-    updatePlaylistById
-}
-
-export default apis
+  createPlaylist,
+  deletePlaylistById,
+  getPlaylistById,
+  getPlaylistPairs,
+  updatePlaylistById,
+};
+export default apis;
